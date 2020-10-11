b38a5c5339ba633ce4aa0e7f9e7dd059bd1af9e7
Mahira <soabmahira@gmail.com>
2020-10-11 22:40:21 +0400

    Add update service to person info

--- client/src/service/PersonService.js
+++ client/src/service/PersonService.js
@@ -5,5 +5,8 @@ class PersonService {
      addPersonInfo(person) {
               return axios.post(`${API_URL}/api/addPerson/`, person);
         }
+        updatePersonInfo(id, person) {
+                  return axios.put(`${API_URL}/api/updatePerson/${id}`, person);
+        }
 }
 export default new PersonService()
