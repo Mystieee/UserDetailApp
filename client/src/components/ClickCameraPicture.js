6582dde266d3d50d3a5efb2cd426b9844ac779a2
Mahira <soabmahira@gmail.com>
2020-10-23 18:22:23 +0400

    Handle capture image through webcam

---
+++ client/src/components/ClickCameraPicture.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "reactjs-popup/dist/index.css";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
+import cameraimage from "../images/camera.png";
+import ImagePreview from "./ImagePreview";
+import "../styles/imagePreview.css";
+
+export default function ClickCameraPicture() {
+  const [dataUri, setDataUri] = useState("");
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  function handleTakePhotoAnimationDone(dataUri) {
+    console.log("takePhoto");
+    setDataUri(dataUri);
+    setIsCameraOpen(false);
+  }
+  const isFullscreen = false;
+
+  return (
+    <div>
+      <div class="image-upload">
+        <a onClick={() => setIsCameraOpen(true)}>
+          <img src={cameraimage} alt="open camera" />
+        </a>
+        {isCameraOpen && (
+          <Camera
+            onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+            isFullscreen={isFullscreen}
+          />
+        )}
+
+        {!isCameraOpen && (
+          <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} />
+        )}
+      </div>
+    </div>
+  );
+}
